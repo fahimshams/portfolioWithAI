dfdb25cef2f3923d08bbc63dacebd7168327bfec
Fahim Shams <fahimshams@my.unt.edu>
2024-02-28 23:16:28 -0500

    Intro part completion

--- myportfolio/src/intro-section/Intro.js
+++ myportfolio/src/intro-section/Intro.js
@@ -1,13 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './Intro.css';
 import logo1 from './images/front-end-logo.png';
-import Avatar, { genConfig } from 'react-nice-avatar';
+import profile1 from './images/profile1.jpg';
+import profile2 from './images/profile2.jpg';
+import profile3 from './images/profile3.jpg';
+import profile4 from './images/profile4.jpg';
+import { FaLinkedin, FaFacebook, FaInstagram, FaGithub } from 'react-icons/fa';
+
 
 const Intro = () => {
 
 
-    const config = genConfig({ shirtStyle: "polo", glassesStyle: " none", hairColor: "black", shape: "round", sex: "man", hairStyle: "normal", faceColor: "white" })
+    const [currentIndex, setCurrentIndex] = useState(0);
 
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setCurrentIndex(prevIndex => (prevIndex + 1) % 4);
+        }, 3000);
+
+        return () => clearInterval(interval);
+    }, []);
+
+    const images = [profile1, profile2, profile3, profile4];
 
 
     return (
@@ -19,14 +33,37 @@ const Intro = () => {
                 <span>Hi</span>
 
                 <p>My name is Fahim Ibne Shams!</p>
+
+                <div className="social-icons">
+                    <p style={{ marginBottom: '30px' }}>Visit my social media sites:</p>
+                    <div className="icon-container">
+                        <div >
+                            <a href="https://www.linkedin.com/in/fis/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                            <p>LinkedIn</p>
+                        </div>
+                        <div >
+                            <a href="https://www.facebook.com/fahim.pranjol/" target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+                            <p>Facebook</p>
+                        </div>
+                        <div >
+                            <a href="https://www.instagram.com/fahimpranjol/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+                            <p>Instagram</p>
+                        </div>
+                        <div >
+                            <a href="https://github.com/fahimshams" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                            <p>GitHub</p>
+                        </div>
+                    </div>
+                </div>
             </h1>
 
-            <div className="avatar-container">
-                <Avatar style={{ width: '8rem', height: '8rem' }} {...config} />
 
-                {/* Your animated avatar component goes here */}
-                {/* Example: <AnimatedAvatar /> */}
+
+
+            <div className="image-container">
+                <img style={{ width: '30rem', height: '28rem', marginTop: '20px' }} src={images[currentIndex]} alt='Fahim Shams'></img>
             </div>
+
         </div>
 
     )
--- myportfolio/src/navbar/Navbar.js
+++ myportfolio/src/navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
     return (
         <nav className={`navbar ${isVisible ? 'active' : ''}`}>
             <div className='container'>
-                <div className='logo'>Take a jounrey into my life!</div>
+                <div className='logo'>Fahim's Portfolio</div>
                 <ul className={`nav-links ${isOpen ? "active" : ""}`}>
                     <li><a href="#home">Home</a></li>
                     <li><a href="#about">About</a></li>
